30545e8d9bd2a42434bd28431155984e61f4af96
Kojoel <nanaabruquah1s@gmail.com>
2024-07-17 04:55:57 -0700

    Status bar component UI completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StatusBarComponent } from './status-bar/status-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StatusBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
