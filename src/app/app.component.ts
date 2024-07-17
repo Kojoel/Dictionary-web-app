19ef087248ac8d2573b25973c7371df13ac18453
Kojoel <nanaabruquah1s@gmail.com>
2024-07-17 08:03:38 -0700

    Updated word gen style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StatusBarComponent } from './status-bar/status-bar.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { WordGenComponent } from './word-gen/word-gen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StatusBarComponent, SearchBarComponent],
+  imports: [RouterOutlet, StatusBarComponent, SearchBarComponent, WordGenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
