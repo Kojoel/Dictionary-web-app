c4a9607dccd11320446c5c113a3ec8889de2816e
Kojoel <nanaabruquah1s@gmail.com>
2024-07-18 10:28:43 -0700

    API call completed

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { ThemeService } from '../service/theme.service';
 import { FontService } from '../service/font.service';
 import { FormsModule } from '@angular/forms';
+import { DataService } from '../service/data.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -13,11 +16,22 @@ import { FormsModule } from '@angular/forms';
 export class SearchBarComponent {
   darkmode = inject(ThemeService);
   font = inject(FontService);
-
+  data = inject(DataService);
   value = 'lakjsdf';
+  
+  private apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/'; 
 
-  getValue() {
-    console.log(this.value);
-    this.value = '';
+  constructor(private http: HttpClient) { }
+
+  fetchData() {
+     this.http.get(this.apiUrl + this.value).subscribe((response) => {
+      this.data.returnedData = response;
+      console.log(this.data.returnedData);
+     });
   }
+
+  // getValue() {
+  //   // console.log(this.apiUrl + this.value);
+  //   this.fetchData.subscribe()
+  // }
 }
